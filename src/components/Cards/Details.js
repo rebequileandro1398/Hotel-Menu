ad7e4f46619483f7c830ba2e6d07e1d888a0bc8a
Leandro Rebequi <71858342+rebequileandro1398@users.noreply.github.com>
2022-02-26 15:13:27 -0300

    details

--- src/components/Cards/Details.js
+++ src/components/Cards/Details.js
@@ -7,9 +7,16 @@ export const Details = () => {
   return (
     <div>
         {   detailsItem.length !== 0 ?
-                <div className="card mb-3">
-                    <h1>{detailsItem.title}</h1>
-                    <img src={detailsItem.images[2]} className="card-img-top" alt="food"/>
+                <div className="card bg-dark text-white" style={{width: '40rem'}}>
+                    <img style={{opacity: '0.5'}} src={detailsItem.images[2]} className="card-img" alt="food"/>
+                    <div className='card-img-overlay'>
+                    <h1 className='card-title'>{detailsItem.title}</h1>
+                        <h3>Información nutricional</h3>
+                        <h6>Calorías: {detailsItem.nutrition.calories}</h6>
+                        <h6>Grasas totales: {detailsItem.nutrition.fat}</h6>
+                        <h6>Proteínas: {detailsItem.nutrition.protein}</h6>
+                        <h6>Carbohidratos: {detailsItem.nutrition.carbs}</h6>
+                    </div>
                 </div>
             :
             <img src={foodError} alt='404 food no found'/>
